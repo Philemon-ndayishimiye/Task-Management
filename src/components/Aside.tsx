4f8ae13f732bc6cdcbceebcef4cfcfcd0c53c2db
Philemon-ndayishimiye <philemonndayi@gmail.com>
2025-08-09 20:45:03 +0200

    complete page

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IoAdd } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { MdOutlineIncompleteCircle } from "react-icons/md";
+import { GrCompliance } from "react-icons/gr";
 
 export default function Aside() {
   return (
@@ -24,6 +25,17 @@ export default function Aside() {
           </div>
           <h1 className="font-bold text-blue-800 text-lg hover:text-white">
             Incomplete
+          </h1>
+        </div>
+      </Link>
+
+      <Link to="/incomplete">
+        <div className="flex py-1 px-3 gap-3 mt-10 cursor-pointer hover:bg-blue-500 hover:text-white">
+          <div className="text-2xl">
+            <GrCompliance />
+          </div>
+          <h1 className="font-bold text-blue-800 text-lg hover:text-white">
+            Complete
           </h1>
         </div>
       </Link>
